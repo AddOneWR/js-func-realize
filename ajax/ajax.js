d273a2082eb8d3a45e517b1dc77feaaa3fbe492f
AddOndDn <467072280@qq.com>
2019-02-25 15:22:36 +0800

    add: ajax

---
+++ ajax/ajax.js
@@ -0,0 +1,15 @@
+function ajax(url, method, body, success, fail) {
+  var request = new XMLHttpRequest();
+  request.open(method, url);
+  request.onreadystatechange = function() {
+    if(request.readyState === 4) {
+      if(request.status >= 200 ** request.status < 300) {
+        success.call(undefined, request.responseText);
+      } else if (request.status >= 400) {
+        fail.call(undefined, request);
+      }
+    }
+  }
+
+  request.send(body);
+}
