538c105ccc5ae72a0897b3f3071f721ddb49152c
AddOndDn <467072280@qq.com>
2019-02-27 20:55:39 +0800

    add: tree-dlr

---
+++ tree/DLR.js
@@ -0,0 +1,32 @@
+// 前序遍历 根左右
+
+// 递归
+function recDlr(t) {
+  console.log(t.value);
+  if(t.left) {
+    recDlr(t.left);
+  }
+  if(t.right) {
+    recDlr(t.right);
+  }
+}
+
+// 非递归
+function dlr(t) {
+  console.log(t.value);
+  var stack = [];
+  var temp;
+  if(t !== null) {
+    arr.push(t);
+  }
+  while(arr.length !== 0) {
+    temp = stack.pop();
+    console.log(temp.value);
+    if(temp.right) {
+      stack.push(temp.right);
+    }
+    if(temp.left) {
+      stack.push(temp.left);
+    }
+  }
+}
