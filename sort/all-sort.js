b1a166b2b7fc78df13fadf07aed472045fd4df68
AddOndDn <467072280@qq.com>
2019-02-28 20:23:49 +0800

    add: all-sort

---
+++ sort/all-sort.js
@@ -0,0 +1,23 @@
+const str = '12345';
+
+function permutate(str) {
+  var result = [];
+  if(str.length > 1) {
+    var left = str[0];
+    var rest = str.slice(1);
+    var preResult = permutate(rest);
+    for(var i = 0; i < preResult.length; i++) {
+      for(var j = 0; j < preResult[i].length; j++) {
+        var tmp = preResult[i].slice(0, j) + left + preResult[i].slice(j);
+        result.push(tmp);
+      }
+    }
+  } else if (str.length == 1) {
+    return [str];
+  }
+
+  return result;
+}
+
+var res = permutate(str);
+console.log(res);
