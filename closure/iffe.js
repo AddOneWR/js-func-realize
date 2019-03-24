1f8c612ef2a35960a0d4b331b5189faf4800b2ec
AddOndDn <467072280@qq.com>
2019-03-24 21:10:15 +0800

    add: closure/iffe

--- closure/iffe.js
+++ closure/iffe.js
@@ -1,9 +1,40 @@
 for (var i = 0; i < 3; i++) {
-  (function(num) {
-      setTimeout(function() {
-          console.log(num);
-      }, 1000);
+  // 闭包传参
+  ((i) => {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
   })(i);
+}
+
+for (var i = 0; i < 3; i++) {
+  // 闭包传参，但是因为传入立即执行函数，不会等1s后打印
+  setTimeout(((i) => {
+    console.log(i);
+  })(i), 1000);
+}
+
+// 对于上者的iffe，这里iffe中返回一个函数
+for (var i = 0; i < 3; i++) {
+  setTimeout(((i) => {
+    return function() {
+      console.log(i);
+    }
+  })(i), 1000);
+}
+
+for (var i = 0; i < 3; i++) {
+  // setTimeout传参
+  setTimeout((i) => {
+    console.log(i)
+  }, 1000, i);
+}
+
+for (let i = 0; i < 3; i++) {
+  // setTimeout传参
+  setTimeout((i) => {
+    console.log(i)
+  }, 1000, i);
 }
 // 0
 // 1
