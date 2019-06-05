c537473209e871781f3f19ae0e08840f7f617216
AddOndDn <467072280@qq.com>
2019-06-05 09:55:46 +0800

    fix: 修改 class-func-chain 逻辑

--- this/class-func-chain.js
+++ this/class-func-chain.js
@@ -1,64 +1,53 @@
-class LazyManClass {
+class ManagerClass {
   constructor(name) {
-    this.name = name;
     this.taskList = [];
-    console.log(`Hi I am ${this.name}`);
-    setTimeout(() => {
-      this.next();
-    }, 0);
-  }
-
-  eat(name) {
-    let that = this;
-    let fn = (function() {
-      return function() {
-        console.log(`I am eating ${name}`);
-        that.next();
-      }
-    })(name);
-
-    this.taskList.push(fn);
-    return this;
-  }
-
-  sleep(time) {
-    let that = this;
-    let fn = (function() {
-      return function() {
-        setTimeout(() => {
-          console.log(`等待了${time}秒...`);
-          that.next();
-        }, time * 1000)
-      }
-    })(time);
-
-    this.taskList.push(fn);
-    return this;
+    console.log(`Hi I am ${name}`);
+    setTimeout(() => this.next(), 0);
   }
 
   sleepFirst(time) {
-    let that = this;
-    let fn = (function() {
-      return function() {
-        setTimeout(() => {
-          console.log(`等待了${time}秒...`);
-          that.next();
-        }, time * 1000)
-      }
-    })(time);
+    let fn = () => {
+      console.log(`Sleep ${time}s...`);
+      setTimeout(() => {
+        this.next();
+      }, time * 1000);
+    }
 
     this.taskList.unshift(fn);
     return this;
   }
 
+  sleep(time) {
+    let fn = () => {
+      console.log(`Sleep ${time}s...`);
+      setTimeout(() => {
+        this.next();
+      }, time * 1000);
+    }
+
+    this.taskList.push(fn);
+    return this;
+  }
+
+  eat(name) {
+    let fn = () => {
+      console.log(`I am eating ${name}`);
+      this.next();
+    }
+
+    this.taskList.push(fn);
+    return this;
+  }
+
   next() {
     let fn = this.taskList.shift();
+
     fn && fn();
   }
 }
 
-function LazyMan(name) {
-  return new LazyManClass(name);
+function Manager(name) {
+  return new ManagerClass(name);
 }
 
-LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(2).sleep(1).eat('junk food');
+Manager('AddOne').eat('breakfast').eat('lunch').sleepFirst(2).sleep(1).eat('dinner');
